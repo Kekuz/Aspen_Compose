9f4e84185d2065deafcedef7324ecbbf66eccbc8
Kekuz <anton.edlenko@yandex.ru>
2024-02-27 13:08:47 +0300

    Настроил шрифты и сделал верстку первого экрана

--- app/src/main/java/com/aspen_compose/MainActivity.kt
+++ app/src/main/java/com/aspen_compose/MainActivity.kt
@@ -3,44 +3,142 @@ package com.aspen_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aspen_compose.ui.theme.Aspen_ComposeTheme
+import com.aspen_compose.ui.theme.circularFamily
+import com.aspen_compose.ui.theme.hiatusFamily
+import com.aspen_compose.ui.theme.montserratFamily
+import com.aspen_compose.ui.theme.travel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Aspen_ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GreetingPreview()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Background(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.background_picture),
+        contentDescription = "background",
+        modifier = modifier,
+        contentScale = ContentScale.Crop
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Title() {
+    Text(
+        text = stringResource(id = R.string.aspen),
+        modifier = Modifier.padding(top = 93.dp),
+        fontSize = 116.sp,
+        color = colorResource(id = R.color.white),
+        fontFamily = hiatusFamily,
+    )
+}
+
+
+@Composable
+fun BottomText(text: String, fontSize: TextUnit, fontWeight: FontWeight? = null) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp),
+        text = text,
+        fontSize = fontSize,
+        fontWeight = fontWeight,
+        color = colorResource(id = R.color.white),
+        fontFamily = montserratFamily,
+    )
+}
+
+@Composable
+fun ExploreButton() {
+    Button(
+        onClick = {
+            //TODO Навигацию в следующий экран
+        },
+        modifier = Modifier
+            .padding(bottom = 48.dp, top = 32.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 32.dp),
+        colors = ButtonDefaults.buttonColors(travel),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.explore),
+            Modifier.padding(vertical = 8.dp),
+            fontFamily = circularFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+        )
+    }
+}
+
+
+@Preview(device = "spec:width=411dp,height=891dp")
 @Composable
 fun GreetingPreview() {
     Aspen_ComposeTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Background(Modifier)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Title()
+                Spacer(modifier = Modifier.weight(1f))
+                BottomText(
+                    text = stringResource(id = R.string.plan_your),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Normal
+                )
+                BottomText(
+                    text = stringResource(id = R.string.luxurious),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                BottomText(
+                    text = stringResource(id = R.string.vacation),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Medium
+                )
+                ExploreButton()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/aspen_compose/ui/theme/Type.kt
+++ app/src/main/java/com/aspen_compose/ui/theme/Type.kt
@@ -2,11 +2,13 @@ package com.aspen_compose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.aspen_compose.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +17,42 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+
+
+val montserratFamily = FontFamily(
+    Font(R.font.montserrat_thin, FontWeight.Thin),
+    Font(R.font.montserrat_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.montserrat_extra_light, FontWeight.ExtraLight),
+    Font(R.font.montserrat_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+    Font(R.font.montserrat_light, FontWeight.Light),
+    Font(R.font.montserrat_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold),
+    Font(R.font.montserrat_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.montserrat_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.montserrat_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.montserrat_black, FontWeight.Black),
+    Font(R.font.montserrat_black_italic, FontWeight.Black, FontStyle.Italic),
+)
+
+val hiatusFamily = FontFamily(
+    Font(R.font.hiatus, FontWeight.Normal),
+)
+
+val circularFamily = FontFamily(
+    Font(R.font.figtree_light, FontWeight.Light),
+    Font(R.font.figtree_regular, FontWeight.Normal),
+    Font(R.font.figtree_medium, FontWeight.Medium),
+    Font(R.font.figtree_semi_bold, FontWeight.SemiBold),
+    Font(R.font.figtree_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.figtree_bold, FontWeight.Bold),
+    Font(R.font.figtree_black, FontWeight.Black),
+
 )
